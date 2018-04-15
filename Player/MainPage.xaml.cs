7ed758b382b080187ee4e69fb674cbdd3f847f29
Hanahaaa <444316730@qq.com>
2018-04-15 21:10:50 +0800

    naive player 2

--- Player/MainPage.xaml.cs
+++ Player/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -28,6 +29,7 @@ namespace Player
     public sealed partial class MainPage : Page
     {
         MediaPlayer myplayer=new MediaPlayer();
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -48,18 +50,53 @@ namespace Player
                 //mediaPlayer.Source = mediaSource;
                 mediaPlayer.Source = MediaSource.CreateFromStorageFile(file);
                 mediaPlayer.Visibility = Visibility.Visible;
-                button.Visibility = Visibility.Collapsed;
+                ButtonOpen.Visibility = Visibility.Collapsed;
             }
             else
             {
                 FileText.Text = "未选择文件";
             }
         }
-        
-        private async void Button_Click(object sender,RoutedEventArgs e)
+
+        private void Button_Click_Open(object sender, RoutedEventArgs e)
         {
             openFile();
             mediaPlayer.SetMediaPlayer(myplayer);
+        }
+
+        private void Button_Click_On(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Source = MediaSource.CreateFromUri(new Uri("http://www.neu.edu.cn/indexsource/neusong.mp3"));
+
+        }
+
+        public async Task<StorageFile> Load()
+        {
+            try
+            {
+                Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+                var buffer = await httpClient.GetBufferAsync(new Uri("http://www.neu.edu.cn/indexsource/neusong.mp3"));
+                if (buffer != null && buffer.Length > 0u)
+                {
+                    var file2 = await KnownFolders.MusicLibrary.CreateFileAsync("neusong.mp3", CreationCollisionOption.ReplaceExisting);
+                    using (var stream = await file2.OpenAsync(FileAccessMode.ReadWrite))
+                    {
+                        await stream.WriteAsync(buffer);
+                        await stream.FlushAsync();
+                    }
+
+                    mediaPlayer.Source = MediaSource.CreateFromStorageFile(file2);
+                    return file2;
+                }
+            }
+            catch { }
+            return null;
+        }
+
+        private async void Button_Click_DownL(object sender, RoutedEventArgs e)
+        {
+            await Load();   
+
         }
     }
 }
